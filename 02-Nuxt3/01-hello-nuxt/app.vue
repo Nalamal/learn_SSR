cbd18359a415d5341ed45f80202b367356865c51
贺煜林 <1981766322@qq.com>
2023-06-03 23:59:06 +0800

    feat: create nuxt project

---
+++ 02-Nuxt3/01-hello-nuxt/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <!-- 组件库的组件 nuxt/ui -->
+    <NuxtWelcome />
+  </div>
+</template>
+<script setup>
+// 监听全局路由
+const router = useRouter();
+// router.beforeEach((to, from) => {});
+</script>
