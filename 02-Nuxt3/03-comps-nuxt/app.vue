3af3f86fa7ca6c44a8257074e85e0f3efcf5d02c
贺煜林 <1981766322@qq.com>
2023-06-04 14:34:38 +0800

    feat: add nuxt component

---
+++ 02-Nuxt3/03-comps-nuxt/app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>App 根组件</h1>
+    <!-- 是对 router-view 封装 -->
+    <NuxtPage></NuxtPage>
+  </div>
+</template>
---
+++ 02-Nuxt3/03-comps-nuxt/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <div>我是Home Page</div>
+
+    <!-- <ClientOnly fallback-tag="h3" fallback="loading....">
+      <div>我只会在 client 中渲染</div>
+    </ClientOnly> -->
+
+    <ClientOnly>
+      <div>我只会在 client 中渲染</div>
+      <template #fallback>
+        <h3>服务端渲染的 loading 元素</h3>
+      </template>
+    </ClientOnly>
+  </div>
+</template>
