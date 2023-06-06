954f228f26660c499ff65e0367d8d7397decd246
贺煜林 <1981766322@qq.com>
2023-06-06 15:36:31 +0800

    feat: add nuxt lifecycle

---
+++ 02-Nuxt3/10-lifecycle-nuxt/pages/cart.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>cart</div>
+</template>
+
+<script lang="ts" setup>
+console.log("setup");
+onBeforeMount(() => {
+  console.log("onBeforeMount");
+});
+onMounted(() => {
+  console.log("onMounted");
+});
+</script>
